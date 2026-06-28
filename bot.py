75837f0a38443bbb2d95fbad73b1a3a0872f5974
mukxabek2010-droid <mukxabek2010@gmail.com>
2026-06-28 13:53:59 +0500

    Delete bot.py

--- bot.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN   = os.getenv("BOT_TOKEN", "")
-ADMIN_IDS   = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
-CHANNEL_1   = os.getenv("CHANNEL_1", "@kanal1")
-CHANNEL_2   = os.getenv("CHANNEL_2", "@kanal2")
-WEBHOOK_URL=https://sizning-app.onrender.com
-PORT=8000
