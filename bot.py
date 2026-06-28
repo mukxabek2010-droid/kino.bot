ece8a2d39fef843975fffc7fb8224684177ca7d9
mukxabek2010-droid <mukxabek2010@gmail.com>
2026-06-28 13:47:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-BOT_TOKEN=token_shu_yerga
-ADMIN_IDS=123456789
-CHANNEL_1=@kanal1
-CHANNEL_2=@kanal2
+BOT_TOKEN   = os.getenv("BOT_TOKEN", "")
+ADMIN_IDS   = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+CHANNEL_1   = os.getenv("CHANNEL_1", "@kanal1")
+CHANNEL_2   = os.getenv("CHANNEL_2", "@kanal2")
 WEBHOOK_URL=https://sizning-app.onrender.com
 PORT=8000
